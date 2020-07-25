faac02ade9ca998562f602307e08946559ae6289
WillyMosquera <68791577+WillyMosquera@users.noreply.github.com>
2020-07-25 15:35:12 -0500

    Add files via upload

---
+++ js/Efecto-Acordeon.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+//Acordeon
+
+  $(".Acordeon").accordion();
+
+
+});
---
+++ js/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(main);
+
+var contador=1;
+
+function main() {
+  $('.menu_bar').click(function(){
+
+    $('nav').slideToggle(1000);
+
+  });
+
+}
